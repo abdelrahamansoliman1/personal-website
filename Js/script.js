7ec56c391a615c8bd5b173c799bd122341ae3d85
abdelrahamansoliman1 <abood.hawas1@gmail.com>
2025-10-14 17:21:04 +0200

    my first website

---
+++ Js/script.js
@@ -0,0 +1,6 @@
+window.addEventListener('DOMContentLoaded', function() {
+            setTimeout(function() {
+            document.getElementById('profilePicWrapper').classList.add('show-profile-pic');
+            }, 300); 
+});
+
